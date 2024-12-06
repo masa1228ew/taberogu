a60047631045c00d3c71371a1f60e747ad73019e
masa1228ew <masapapi@icloud.com>
2024-12-07 00:10:53 +0900

    カテゴリ一覧

--- taberogu/taberogu/src/main/java/com/example/taberogu/entity/Category.java
+++ taberogu/taberogu/src/main/java/com/example/taberogu/entity/Category.java
@@ -1,13 +1,10 @@
 package com.example.taberogu.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -24,6 +21,6 @@ public class Category {
 	 @Column(name="category")
 	 private String category;
 	 
-	  @OneToMany(mappedBy = "category")
-	    private List<Shop> shops;
+//	  @OneToMany(mappedBy = "category")
+//	    private List<Shop> shops;
 }
